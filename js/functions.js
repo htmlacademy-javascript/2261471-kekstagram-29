892952e03cd2ee84cfa0fdbf6f36bb0edb04dc21
Danil <Vsadnik93@mail.ru>
2023-06-13 15:23:26 +0300

    Делает две функции из дз

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,22 @@
-function checkLength (anystring, number) {
-  return anystring.length <= number;
+// function checkLength (anystring, number) {
+//   return anystring.length <= number;
+// }
+
+// checkLength();
+
+function checkPalindrom (string) {
+  const stringForward = string.replaceAll(' ', '').toLowerCase();
+  let stringReverse = '';
+
+  for (let i = stringForward.length - 1; i >= 0; i--) {
+    stringReverse += stringForward.at(i);
+  }
+
+  if (stringForward === stringReverse) {
+    return 'Палиндромище!';
+  } else {
+    return 'Не в этот раз';
+  }
 }
 
-console.log(checkLength('Привет мир который скоро что-то узнает', 454));
-
+console.log(checkPalindrom(' А роза упала на лапу азора'));
