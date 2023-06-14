874756e3c4e91e7ff97b353dc4f613500893e5e4
Danil <Vsadnik93@mail.ru>
2023-06-14 20:24:19 +0300

    Добавляет ещё одну функцию

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,9 @@
-// function checkLength (anystring, number) {
-//   return anystring.length <= number;
-// }
+// eslint-disable-next-line no-unused-vars
+const checkLength = (anystring, number) => anystring.length <= number;
 
-// checkLength();
+checkLength('Что делал слон когда пришёл, 15');
 
-function checkPalindrom (string) {
+const checkPalindrom = (string) => {
   const stringForward = string.replaceAll(' ', '').toLowerCase();
   let stringReverse = '';
 
@@ -12,11 +11,17 @@ function checkPalindrom (string) {
     stringReverse += stringForward.at(i);
   }
 
-  if (stringForward === stringReverse) {
-    return 'Палиндромище!';
-  } else {
-    return 'Не в этот раз';
-  }
-}
+  return (stringForward === stringReverse);
+};
 
-console.log(checkPalindrom(' А роза упала на лапу азора'));
+checkPalindrom(' А роза упала на лапу азора');
+
+const createNumber = (string) => {
+  if (string && string.length === 0) {
+    return NaN;
+  }
+
+  return parseInt(string.replace(/\D+/g, ''), 10);
+};
+
+createNumber('33 коровы, ни одного быка');
