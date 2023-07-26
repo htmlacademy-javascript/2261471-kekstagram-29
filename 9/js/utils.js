856c7a4e3e6de002545100f699b3f02cd8e89082
Keks <keks@htmlacademy.ru>
2023-07-26 22:12:09 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/utils.js
@@ -0,0 +1,11 @@
+// Функция для нахождения случайного числа
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+// Функция проверки клавиши Esc
+// const isEscapeKey = (evt) => {
+//   // if(evt.key === Keys.ESCAPE || evt.key === Keys.ESC) {}
+// };
+
+const normalizeString = (string) => string.toLowerCase().trim();
+
+export {getRandomInteger, normalizeString};
