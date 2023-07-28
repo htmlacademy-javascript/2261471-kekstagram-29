5db0d1edf1c9d50c30671012c7cf4620bc655617
Danil <Vsadnik93@mail.ru>
2023-07-28 17:22:16 +0300

    Делает кнопки изменения масштаба

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 import './data.js';
-import './form-validate.js';
+// import './form-validate.js';
 import './utils.js';
 import './loader.js';
 import './mini-pictures.js';
 import './big-picture.js';
+import './scale.js';
 
 import {renderPictures} from './mini-pictures.js';
 import {addPhotos} from './data.js';
