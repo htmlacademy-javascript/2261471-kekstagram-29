a86a1470309d6d07e9f933eea910a5edbb661440
Danil <Vsadnik93@mail.ru>
2023-07-30 21:56:20 +0300

    Добавляет загрузку с сервера

--- js/main.js
+++ js/main.js
@@ -6,12 +6,36 @@ import './loader.js';
 import './mini-pictures.js';
 import './big-picture.js';
 import './scale.js';
+import './server.js';
 
 import {renderPictures} from './mini-pictures.js';
 import {addPhotos} from './data.js';
 import {onFormSubmit} from './form-validate.js';
+import { getData, sendData } from './server.js';
 
-renderPictures(addPhotos());
+// renderPictures(addPhotos());
+
+let photos = [];
+
+const onSuccess = (data) => {
+  photos = data.slice();
+  renderPictures(photos);
+  document.querySelector('.img-filters').classList.remove('img-filters--inactive');
+};
+
+const onError = () => {
+  const messageAlert = document.createElement('div');
+  messageAlert.style.position = 'absolute';
+  messageAlert.style.top = 0;
+  messageAlert.style.left = 0;
+  messageAlert.style.fontSize = '30px';
+  messageAlert.style.textContent = 'Ошибка загрузки';
+  messageAlert.style.color = 'white';
+  messageAlert.style.backgroundColor = 'red';
+  document.body.append(messageAlert);
+};
+
+getData(onSuccess, onError);
 
 onFormSubmit();
 
--- js/server.js
+++ js/server.js
@@ -1,1 +1,26 @@
-fetch('https://29.javascript.pages.academy/kekstagram/data');
+const Route = {
+  GET: 'https://29.javascript.pages.academy/kekstagram/data',
+  POST: 'https://29.javascript.pages.academy/kekstagram',
+};
+
+const sendRequest = (onSuccess, onError, method, body) => {
+  fetch(
+    Route[method],
+    {
+      method: method,
+      body: body
+    },
+  )
+    .then((response) => response.json())
+    .then((data) => onSuccess(data))
+    .catch((err) => {
+      onError(err);
+    });
+};
+
+// Функция получения данных
+const getData = (onSuccess, onError, method = 'GET') => sendRequest(onSuccess, onError, method);
+// Функция отправки данных
+const sendData = (onSuccess, onError, method = 'POST', body) => sendRequest(onSuccess, onError, method, body);
+
+export {getData, sendData};
